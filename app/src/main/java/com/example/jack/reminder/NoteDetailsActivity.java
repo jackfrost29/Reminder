e9795fee8df012496456d00cc589f396c2a45717
jack <zakiazad.ops@gmail.com>
2019-01-02 01:54:44 +0600

    2nd item: simple note

---
+++ app/src/main/java/com/example/jack/reminder/NoteDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.example.jack.reminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NoteDetailsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_details);
+    }
+}

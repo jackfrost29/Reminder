60ce222f34e54e20c19aa74368a771d1e492f736
jack <zakiazad.ops@gmail.com>
2019-01-04 23:49:00 +0600

    2nd item: simple note

--- app/src/main/java/com/example/jack/reminder/Item.java
+++ app/src/main/java/com/example/jack/reminder/Item.java
@@ -5,9 +5,9 @@ import java.util.Date;
 public class Item {
     String title, intro;
     Date date;
-    public Item(String title, String intro, Date date){
+    public Item(String title, String intro){
         this.title = title;
         this.intro = intro;
-        this.date = date;
+//        this.date = date;
     }
 }
--- app/src/main/java/com/example/jack/reminder/Note.java
+++ app/src/main/java/com/example/jack/reminder/Note.java
@@ -4,6 +4,6 @@ import java.util.Date;
 
 public class Note extends Item {
     public Note(String title, String intro, Date date) {
-        super(title, intro, date);
+        super(title, intro);
     }
 }
